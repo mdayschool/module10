bd4d4f6a1370a80241bea3926cb3863e61b8e860
Michael Day <day.michael.b@gmail.com>
2019-07-10 18:58:55 -0500

    object created all attributes green

--- test/test_student.py
+++ test/test_student.py
@@ -3,15 +3,24 @@ from class_definitions import student as s
 
 class StudentTestCase(unittest.TestCase):
     def setUp(self):
-        self.test_student = s.Student('Day', 'Michael', 'CIS')
+        self.t_student = s.Student('Day', 'Michael', 'CIS')
 
     def tearDown(self):
-        del self.test_student
+        del self.t_student
 
     def test_object_created_required_attributes(self):
-        assert self.test_student.last_name == 'Day'
-        assert self.test_student.first_name == 'Michael'
-        assert self.test_student.major == 'CIS'
-        assert self.test_student.gpa == 0.0
+        assert self.t_student.last_name == 'Day'
+        assert self.t_student.first_name == 'Michael'
+        assert self.t_student.major == 'CIS'
+        assert self.t_student.gpa == 0.0
+
+    def test_object_created_all_attributes(self):
+        l_student = s.Student('Day', 'Michael', 'CIS', 3.92)
+        assert l_student.last_name == 'Day'
+        assert l_student.first_name == 'Michael'
+        assert l_student.major == 'CIS'
+        assert l_student.gpa == 3.92
+
+
 
 
