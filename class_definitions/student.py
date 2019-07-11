326e8bee8b41dad0a0823bccff816a91b7c1d82e
Michael Day <day.michael.b@gmail.com>
2019-07-10 19:19:17 -0500

    object not created error major green

--- class_definitions/student.py
+++ class_definitions/student.py
@@ -3,8 +3,11 @@ class Student:
     """Student class"""
     def __init__(self, lname, fname, major, gpa=0.0):
         name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
+        major_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-")
         if not (name_characters.issuperset(lname)
                 and name_characters.issuperset(fname)):
+            raise ValueError
+        if not major_characters.issuperset(major):
             raise ValueError
         self.last_name = lname
         self.first_name = fname
